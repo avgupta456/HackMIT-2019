4072e3b0763d7545f22a96e99baade093b8ccff9
Abhijit Gupta <avgupta456@gmail.com>
2019-09-15 04:51:07 -0400

    new food metric

--- food_predict.py
+++ food_predict.py
@@ -13,26 +13,29 @@ def get_food_info(foods):
     #index 48 is grams in one serving
     #index 3 is the calories in one serving
 
-    #change the healthy_value based on what you think the cal/g for healthiness is
-    healthy_value = 30
-
     all_food = all_parsed_food.drop([0])
 
     #convert index 3 and 48 for int
-    all_food = all_food.astype({3: float, 5: float, 9: float, 15: float, 48: float})
-    all_food = all_food.replace([inf, -inf], nan)
+    all_food = all_food.astype({2: float, 3: float, 4: float, 5: float, 6: float, 7: float, 8: float, 9: float, 15: float})
+    all_food = all_food.replace([np.inf, -np.inf], np.nan)
     all_food = all_food.fillna(0)
 
     #cal/g is the amount of calories per gram of food
-    all_food["cal/g"] = all_food[3]/all_food[48]
-    all_food["fat/g"] = all_food[5]/all_food[48]
-    all_food["sugar/g"] = all_food[9]/all_food[48]
-    all_food["salt/g"] = all_food[15]/all_food[48]/1000 #mg not g
-    all_food["score"] = (all_food["fat/g"])/7.25+(all_food["sugar/g"])/1.75+(all_food["salt/g"])/0.6
-    all_food["healthy"] = ["yes" if x<0.5 else "no" for x in all_food["score"]]
+    all_food["weight"] = all_food[2]+all_food[4]+all_food[5]+all_food[6]+all_food[7]+all_food[8]+all_food[9]
 
-    #determines if a food is healthy or not
-    #all_food["healthy"] = ['yes' if x <= healthy_value else 'no' for x in all_food["cal/g"]]
+    all_food["cal/g"] = all_food[3]/all_food["weight"]*100
+
+    all_food["fat/g"] = all_food[5]/all_food["weight"]*100
+    all_food["fat/g"] = [1 if x > 8 else 0 for x in all_food["fat/g"]]
+
+    all_food["sugar/g"] = all_food[9]/all_food["weight"]*100
+    all_food["sugar/g"] = [1 if x > 40 else 0 for x in all_food["sugar/g"]]
+
+    all_food["salt/g"] = all_food[15]/all_food["weight"]/10 #mg not g
+    all_food["salt/g"] = [1 if x > 1 else 0 for x in all_food["salt/g"]]
+
+    all_food["total"] = all_food["fat/g"] + all_food["sugar/g"] + all_food["salt/g"]
+    all_food["healthy"] = ["yes" if x==0 else "no" for x in all_food["total"]]
 
     healthiness_food = all_food.drop(columns=[0,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52])
     all_desired_details = healthiness_food.iloc[foods,:]
