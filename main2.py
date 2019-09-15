43ae22a129422b6e4a78d6971bc577de79b07e87
Abhijit Gupta <avgupta456@gmail.com>
2019-09-15 05:29:51 -0400

    Create main2.py

---
+++ main2.py
@@ -0,0 +1,36 @@
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+
+import numpy as np
+import scipy as sci
+import pandas as pd
+
+from dataset_map import get_index
+from food_predict import get_food_info
+
+import parse2 as parse
+import abbrev
+import ocr
+
+import os
+import sys
+
+for filename in os.listdir(os.getcwd()+"/data_csv"):
+    if("jpg" in filename):
+        print(filename)
+        data = open("data_csv/"+filename, 'rb')
+        print(data)
+        [items, prices, total] = parse.getItems(data)
+
+        foods_index = get_index(items)
+        food_data = get_food_info(foods_index)
+        food_data['price'] = np.array(prices)
+
+        #comment the below lines if you don't want healthy/unhealthy prices
+        healthy = food_data.loc[food_data['healthy'] == 'yes', 'price'].sum()
+        unhealthy = food_data.loc[food_data['healthy'] == 'no', 'price'].sum()
+
+        print("Healthy: " + str(healthy))
+        print("Unhealthy: " + str(unhealthy))
+        print("Ratio: " + str(healthy/(healthy+unhealthy))[:4])
+        print()
