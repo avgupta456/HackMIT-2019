854d837ebbcdeb51aff42008c483be018382b042
Abhijit Gupta <avgupta456@gmail.com>
2019-09-14 20:50:20 -0400

    working code

--- abbrev.py
+++ abbrev.py
@@ -3,6 +3,12 @@ import enchant, difflib
 d = enchant.Dict("en_US")
 
 def complete(str):
+    out = ""
+    for s in str.split(" "):
+        out += __complete__(s) + " "
+    return out
+
+def __complete__(str):
     try:
         dict, max = {}, 0
         a = set(d.suggest(str))
