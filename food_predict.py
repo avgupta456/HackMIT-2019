28209463a657771670533df4a923145df8bc10c6
Abhijit Gupta <avgupta456@gmail.com>
2019-09-15 03:03:05 -0400

    upgraded food health metric

--- food_predict.py
+++ food_predict.py
@@ -8,7 +8,7 @@ import csv
 def get_food_info(foods):
     #read food data
     path_to_file = './ABBREV.csv'
-    all_parsed_food = pd.read_csv(path_to_file,delimiter=",", header = None)
+    all_parsed_food = pd.read_csv(path_to_file, delimiter=",", header = None)
 
     #index 48 is grams in one serving
     #index 3 is the calories in one serving
@@ -17,12 +17,22 @@ def get_food_info(foods):
     healthy_value = 30
 
     all_food = all_parsed_food.drop([0])
+
     #convert index 3 and 48 for int
-    all_food = all_food.astype({3: float, 48: float})
+    all_food = all_food.astype({3: float, 5: float, 9: float, 15: float, 48: float})
+    all_food = all_food.replace([inf, -inf], nan)
+    all_food = all_food.fillna(0)
+
     #cal/g is the amount of calories per gram of food
     all_food["cal/g"] = all_food[3]/all_food[48]
+    all_food["fat/g"] = all_food[5]/all_food[48]
+    all_food["sugar/g"] = all_food[9]/all_food[48]
+    all_food["salt/g"] = all_food[15]/all_food[48]/1000 #mg not g
+    all_food["score"] = (all_food["fat/g"])/7.25+(all_food["sugar/g"])/1.75+(all_food["salt/g"])/0.6
+    all_food["healthy"] = ["yes" if x<0.5 else "no" for x in all_food["score"]]
+
     #determines if a food is healthy or not
-    all_food["healthy"] = ['yes' if x <= healthy_value else 'no' for x in all_food["cal/g"]]
+    #all_food["healthy"] = ['yes' if x <= healthy_value else 'no' for x in all_food["cal/g"]]
 
     healthiness_food = all_food.drop(columns=[0,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51,52])
     all_desired_details = healthiness_food.iloc[foods,:]
