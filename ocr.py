738e518f6884ef0c48224dc0eb175db4b4251b54
Abhijit Gupta <avgupta456@gmail.com>
2019-09-15 20:28:14 -0400

    remove aws keys

--- ocr.py
+++ ocr.py
@@ -5,8 +5,9 @@ textract = boto3.client(
     'textract',
     # Hard coded strings as credentials, not recommended.
     # TODO: Remove hard coded credentials
-    aws_access_key_id='AKIA32JYDZ7JVYDOOENO',
-    aws_secret_access_key='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', region_name='us-east-1'
+    aws_access_key_id="",#add AWS access key here,
+    aws_secret_access_key="",#add AWS secret access key here, 
+    region_name='us-east-1'
 )
 
 # Use OCR to retrieve text
